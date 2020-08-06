219ab02596add340b74284ccdedeedad1190c237
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-08-06 15:20:09 +0300

    Add files via upload

---
+++ Damsho0ok.js
@@ -0,0 +1,28 @@
+Tsend_=Tsend;
+
+reqlikes=100;
+
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+
+reqlikes=100;
+
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+
+reqlikes=100;
+
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>دمـشقي </span></a>-->
+
+<a href="https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
+
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  آلہـَدمـشۣۗہـقۗـۙيـہ 🕊❥⌁ للتواصل اضغط هنا/a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تعديل و تصميم: <a href='https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/'>  محمد الدمشقي © 2020</a></div>").insertAfter($(".loginstat"));  },1000);
